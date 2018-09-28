8ec7b8474ecee0183c3fb518a222cbf67292f1f0
JaseG256 <jason_r_gibbs@yahoo.com>
2018-09-28 12:19:51 -0400

    Card class added

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,4 +1,51 @@
 import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class Card extends Parent {
+
+    private static final int CARD_WIDTH = 100;
+    private static final int CARD_HEIGHT = 140;
+
+    public final Suit suit;
+    public final Rank rank;
+    public final int value;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = rank.value;
+
+        Rectangle cardBackground = new Rectangle(CARD_WIDTH, CARD_HEIGHT);
+        cardBackground.setArcWidth(20);
+        cardBackground.setArcHeight(20);
+        cardBackground.setFill(Color.WHITE);
+
+        Text text1 = new Text(rank.displayName());
+        text1.setFont(Font.font(18));
+        text1.setX(CARD_WIDTH - text1.getLayoutBounds().getWidth() - 10);
+
+        Text text2 = new Text(text1.getText());
+        text2.setFont(Font.font(18));
+        text2.setX(10);
+        text2.setY(CARD_HEIGHT - 10);
+
+        ImageView imageView = new ImageView(suit.image);
+        imageView.setRotate(180);
+        imageView.setX(CARD_WIDTH - 32);
+        imageView.setY(CARD_HEIGHT - 32);
+
+        getChildren().addAll(cardBackground, new ImageView(
+                suit.image), imageView, text1, text2);
+    }
+
+    @Override
+    public String toString() {
+        return rank.toString() + "of " + suit.toString();
+    }
+
+
 }
