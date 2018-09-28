97063af0059323a7dc9fa18c976e8b1e79202ee6
JaseG256 <jason_r_gibbs@yahoo.com>
2018-09-28 16:13:24 -0400

    CardDeck class added

---
+++ src/main/java/CardDeck.java
@@ -0,0 +1,27 @@
+public class CardDeck {
+
+    private Card[] deck = new Card[52];
+
+    public CardDeck() {
+        refillDeck();
+    }
+
+    public final void refillDeck() {
+        int i = 0;
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                deck[i++] = new Card(suit, rank);
+            }
+        }
+    }
+
+    public final Card takeCard() {
+        Card card = null;
+        while (card == null) {
+            int index = (int)Math.random() * deck.length;
+            card = deck[index];
+            deck[index] = null;
+        }
+        return card;
+    }
+}
